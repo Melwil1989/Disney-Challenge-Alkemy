7fb5211b8656ae5ea8053ec7c6fbac5a97389b11
Melwil1989 <melisawilberg@gmail.com>
2021-11-26 21:19:35 -0300

    first commit

---
+++ src/test/java/ar/com/disneychallenge/DisneychallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.disneychallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneychallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
